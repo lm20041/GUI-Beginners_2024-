1c9852924a5422431f4d7a972540899bfffa53f2
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-03-03 23:16:13 +0000

    07:30-label, textbox

--- main.py
+++ main.py
@@ -5,5 +5,12 @@ root = tk.Tk()
 root.geometry("250,250")
 #title of the window
 root.title("My First GUI")
+#create a label
+label = tk.Label(root, text="Hello World!", fron=('Arial', 18))
+#place the label in the window
+label.pack(padx=20, pady=20)
+#place the textbox in the window
+textbox = tk.Text(root, height=3, font=('Arial', 16))
+textbox.pack()
 
 root.mainloop()
