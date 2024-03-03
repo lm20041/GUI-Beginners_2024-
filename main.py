eb4fcf041ce7b660101e755aeba526f18879d88f
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-03-03 23:23:23 +0000

    08:00-entry
    a different form of textbox

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import tkinter as tk 
 root = tk.Tk() 
 #sides of the window
-root.geometry("250,250")
+root.geometry("2500,2500")
 #title of the window
 root.title("My First GUI")
 #create a label
@@ -12,5 +12,8 @@ label.pack(padx=20, pady=20)
 #place the textbox in the window
 textbox = tk.Text(root, height=3, font=('Arial', 16))
 textbox.pack()
+#place the Entry in the window
+myentry = tk.Entry(root)
+myentry.pack()
 
 root.mainloop()
