eadceea611c65e0dd14094cc72be776e70800983
lm20041 <132313536+lm20041@users.noreply.github.com>
2024-03-05 06:09:25 +0000

    15:50-buttonFrame

--- main.py
+++ main.py
@@ -12,8 +12,24 @@ label.pack(padx=20, pady=20)
 #place the textbox in the window
 textbox = tk.Text(root, height=3, font=('Arial', 16))
 textbox.pack()
-#button
-button = tk.Button(root, text="Click Me!", font=('Arial', 18))
-button.pack(padx=10, pady=10)
+#buttonFrame
+buttonFrame = tk.Frame(root)
+buttonFrame.columnconfigure(0, weight=1)
+buttonFrame.columnconfigure(1, weight=1)
+buttonFrame.columnconfigure(2, weight=1)
+
+btn1 = tk.Button(buttonFrame, text="1", font=('Arial', 18))
+btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
+btn2 = tk.Button(buttonFrame, text="2", font=('Arial', 18))
+btn2.grid(row=0, column=1, sticky=tk.W+tk.E)
+btn3 = tk.Button(buttonFrame, text="3", font=('Arial', 18))
+btn3.grid(row=0, column=2, sticky=tk.W+tk.E)
+btn4 = tk.Button(buttonFrame, text="4", font=('Arial', 18))
+btn4.grid(row=1, column=0, sticky=tk.W+tk.E)
+btn5 = tk.Button(buttonFrame, text="5", font=('Arial', 18))
+btn5.grid(row=1, column=1, sticky=tk.W+tk.E)
+btn6 = tk.Button(buttonFrame, text="6", font=('Arial', 18))
+btn6.grid(row=1, column=2, sticky=tk.W+tk.E)
+buttonFrame.pack(fill='x')
 
 root.mainloop()
